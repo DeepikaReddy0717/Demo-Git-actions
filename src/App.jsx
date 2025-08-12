7cfa25fccb61702c506dc61b22e7159f18d09a14
DeepikaReddy <deepikareddymandapati@gmail.com>
2025-08-12 21:41:21 +0530

    Version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Git Actions Demo 🚀</h1>
+      <p>This is a minimal Vite + React app for GitHub Actions testing.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
